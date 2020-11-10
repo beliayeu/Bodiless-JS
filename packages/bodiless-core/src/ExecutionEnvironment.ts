cd2c3ca203837948e4f180b2f045cf4e6b3836d5
Beliayeu, Andrei [JJCUS Non-J&J] <abeliaye@ITS.JNJ.com>
2020-11-10 16:12:28 +0300

    canUseDOM small fix

--- packages/bodiless-core/src/ExecutionEnvironment.ts
+++ packages/bodiless-core/src/ExecutionEnvironment.ts
@@ -12,7 +12,7 @@
  * limitations under the License.
  */
 
-const canUseDOM = () => (
+const canUseDOM = () => !!(
   typeof window !== 'undefined'
   && window.document
   && window.document.createElement
